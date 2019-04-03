d960d4779b7696ae9fbb21cfc0e36d46c49280de
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-02 22:15:27 -0500

    assignment 1 completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 
-
 module.exports = class MenuController {
     constructor(){
         this.mainMenuQuestions = [
@@ -10,6 +9,7 @@ module.exports = class MenuController {
                 message: "Please choose from an option below: ",
                 choices: [
                     "Add new contact",
+                    "Get Today's Date",
                     "Exit"
                 ]
             }
@@ -26,6 +26,9 @@ module.exports = class MenuController {
                     break;
                 case "Exit":
                     this.exit();
+                case "Get Today's Date":
+                    this.getDate()
+                    break;
                 default:
                     console.log("Invalid Input");
                     this.main();
@@ -49,5 +52,24 @@ module.exports = class MenuController {
     exit(){
         console.log("Thanks for using AddressBloc!");
         process.exit();
+    }
+
+    getDate(){
+        var now = new Date();
+        var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+        var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+        var amPm = (time[0] < 12) ? "AM" : "PM";
+
+        time[0] = (time[0] < 12 ) ? time[0] : time[0] -12;
+        time[0] = time[0] || 12;
+
+        for (var i = 1; i <3; i++){
+            if(time[i] < 10 ){
+                time[i] = "0" + time[i];
+            }
+        }
+        var currentDate = date.join("-") + " " + time.join(":") + " " + amPm;
+        console.log(currentDate);
+        this.main();
     }
 }
