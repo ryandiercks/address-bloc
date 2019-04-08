29ad5a10bc050a720f9c6d915f97d449e47d6bde
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-08 00:22:39 -0500

    assignment-3-creatingv1

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,15 +1,14 @@
-
 const MenuController = require("../controllers/MenuController");
 
 describe("MenuController", () => {
 
-beforeEach(() => {
-    this.menu = new MenuController();
-});
-
-describe("#remindMe", () => {
-    it("should return the string", () => {
-        expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+    beforeEach(() => {
+        this.menu = new MenuController();
     });
-})
+
+    describe("#remindMe", () => {
+        it("should return the string", () => {
+            expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
+        });
+    })
 });
